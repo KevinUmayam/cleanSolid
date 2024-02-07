40c564b53af9dd9af54402cf08f1e92be89c4eef
Kevin Umayam <kevin.m.umayam@gmail.com>
2024-02-07 12:19:27 -0800

    naming conventions for more functions

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -27,6 +27,7 @@
         console.log({ title, description, rating, cast });
     }
 
+    // ! its better to create a new function that takes care of verifying the name and using it in the function below 
     // Crea un nuevo actor
     function createActor( fullName: string, birthdate: Date ): boolean {
         
@@ -37,10 +38,35 @@
 
         console.log('Crear actor', birthdate);
         return true;        
-
     }
 
-    
+// continued 
+//   const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+//         let result;
+//         if ( isDead ) {
+//             result = 1500;
+//         } else {
+//             if ( isSeparated ) {
+//                 result = 2500;
+//             } else {
+//                 if ( isRetired ) {
+//                     result = 3000;
+//                 } else {
+//                     result = 4000; 
+//                 }
+//             }
+//         }
+        
+//         return result;
+//     }
 
+const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }):number => {
 
+    if ( isDead ) return 1500;
+
+    if ( isSeparated ) return 2500;
+
+    return ( isRetired ) ? 3000 : 4000; 
+
+    }
 })();
