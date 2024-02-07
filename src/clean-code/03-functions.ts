162bd5c3b3d520622aa80a286ff78fa505183199
Kevin Umayam <kevin.m.umayam@gmail.com>
2024-02-06 23:49:01 -0800

    naming conventions for more functions

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,46 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieById( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorBioByid ( ActorId: string ) {
+        console.log({ ActorId });
+    }
+    
+
+    interface Movie  {
+    cast: string[] 
+    description: string;
+    rating: number;
+    title: string;
+    }
+    // Crear una película
+    function createMovie({title, description, rating, cast}: Movie ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor', birthdate);
+        return true;        
+
+    }
+
+    
+
+
+})();
