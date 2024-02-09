b20991b276b1d49c340f14e7eddf2158acb52265
Kevin Umayam <kevin.m.umayam@gmail.com>
2024-02-08 22:52:17 -0800

    after applying dry practices to the methods in class product

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -10,14 +10,34 @@ class Product {
         public size: Size = "",
     ) {}
 
-toString(){
-    if(this.name.length <= 0 ) throw Error('name is empty')
-    if(this.price <= 0 ) throw Error('price is empty')
-    if(this.size.length <= 0 ) throw Error('size is empty')
+    isProductReady():boolean {
+     // if(this.name.length <= 0 ) throw Error('name is empty')
+    // if(this.price <= 0 ) throw Error('price is empty')
+    // if(this.size.length <= 0 ) throw Error('size is empty')
+        for(const key in this) {
+        switch (typeof this[key]) {
+            case "string":
+                if((<string><unknown>this[key]).length <= 0 ) throw Error(`${key} is empty`)
+                break;
+
+        case "number":
+                if((<number><unknown>this[key]) <= 0 ) throw Error(`${key} is empty`)
+                break;
+            default:
+                throw Error(`${typeof this[key]} is not valid`)
+                break;
+        }
+    }
+    return true
+    }
+
+toString(): string | undefined{
+    if(!this.isProductReady) return;
     
     return `${this.name} (${this.size}): ${this.price}`
 }
 }
+
 (() => {
 const bluePants = new Product('Blue Pants',20,"L")
 console.log(bluePants.toString());
