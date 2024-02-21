1aabb829f6cc0ba564f4524d3be9fc8a74568bd4
Kevin Umayam <kevin.m.umayam@gmail.com>
2024-02-20 22:58:36 -0800

    added the files to learn how dependency inversion principle works

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,39 @@
+
+interface Bird {
+    fly(): number;
+    eat(): void;
+
+}
+interface FlyingBird {
+    fly(): void;
+}
+
+interface RunningBird {
+    run(): void;
+}
+interface SwimmingBird {
+    swim(): void;
+}
+
+
+class Tucan implements Bird, FlyingBird {
+    public fly(){return 100}
+    public eat(){}
+
+}
+
+class Humminbird implements Bird, FlyingBird {
+    public fly(){return 200}
+    public eat(){}
+}
+
+class Ostrich implements Bird, RunningBird {
+
+    public eat(){}
+    public run(){}
+}
+
+class Penguin implements Bird, SwimmingBird {
+    public eat(){}
+    public swim(){}
+}
